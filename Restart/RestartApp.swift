268ae95f646d1f54c356576a20c8452e4a71c57f
Daria <dtomljenovic@outlook.com>
2023-03-27 09:32:02 +0200

    Initialize git

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by user219285 on 3/24/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
